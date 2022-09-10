654a3ed0ae4cb735e32f948132169138b9b158f8
Forster112 <forstermichael112@gmail.com>
2022-09-10 23:08:29 +0100

    new user and database

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,7 @@
+--File that creates a new database and new user
+--and grants all privileges to the hbnb_dev database
+--just Select privilege to the performance_schema database
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_dev@localhost;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,3 +1,6 @@
+-- File that creates new database hbnb_test_db
+-- And new user hbnb_test with permisions
+
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO hbnb_test@localhost;
