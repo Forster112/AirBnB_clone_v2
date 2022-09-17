1b749f6e440cf434eef35367fbf0aba97060a0fb
Forster112 <forstermichael112@gmail.com>
2022-09-17 19:39:10 +0100

    fix user

--- models/user.py
+++ models/user.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, Base
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from models.place import Place
 from models.review import Review
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+    """This is the class for users credentials"""
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
